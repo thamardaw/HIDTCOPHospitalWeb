cab09e29df41be53a69ed7132a7ccf0fc18a4236
Zay Min Maw <zayminmaw77@gmail.com>
2021-10-27 13:57:16 +0630

    added dashboard UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Login, Signup } from "./pages";
+import { Login, Signup, Dashboard } from "./pages";
 
 function App() {
+  // function parseJwt(token) {
+  //   var base64Url = token.split(".")[1];
+  //   var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  //   var jsonPayload = decodeURIComponent(
+  //     atob(base64)
+  //       .split("")
+  //       .map(function (c) {
+  //         return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+  //       })
+  //       .join("")
+  //   );
+
+  //   return JSON.parse(jsonPayload);
+  // }
+  // console.log(
+  //   parseJwt(
+  //     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ6YXkiLCJyb2xlIjoic3RyaW5nIiwiZXhwIjoxNjM1MjUxNTc2fQ.JWOmIy7J3m8IvguUBjYoARRl684Z6n4RSqHlts9lDO8"
+  //   )
+  // );
   return (
     <Router>
       <Switch>
+        <Route path="/" component={Dashboard} exact />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
       </Switch>
