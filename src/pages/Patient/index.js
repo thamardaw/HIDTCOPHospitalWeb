e6ed4fb7b9e5a185bf91d7420fd7f45395f873aa
Zay Min Maw <zayminmaw77@gmail.com>
2021-11-12 18:18:46 +0630

    removed pages

--- src/pages/Patient/index.js
+++
@@ -1,4 +0,0 @@
-export { default as PatientTable } from "./PatientTable";
-export { default as PatientCreate } from "./PatientCreate";
-export { default as PatientEdit } from "./PatientEdit";
-export { default as PatientDetail } from "./PatientDetail";
--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Login } from "./Login";
-export { default as Signup } from "./Signup";
-export { default as Dashboard } from "./Dashboard";
-export { default as ResetPassword } from "./ResetPassword";
