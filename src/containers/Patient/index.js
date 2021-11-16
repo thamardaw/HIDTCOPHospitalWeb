21fc8a7de26d842d4e95f023554ca4d8e4d0ff7f
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2021-11-16 12:52:44 +0630

    Removed pages folder (#9)
    * fixed spelling error
    
    * added client-side routing
    
    * refactored routes
    
    * added login and Route control
    
    * integrated signup and reset password api
    
    * added axios instance with auto refresh token interceptors for authorize route only
    
    * integrate api to patient table
    
    * added Patient CRUD API integration
    
    * removed pages

--- src/containers/Patient/index.js
+++ src/containers/Patient/index.js
@@ -1,5 +1,4 @@
 export { default as PatientTable } from "./PatientTable";
 export { default as PatientForm } from "./PatientForm";
-export { default as PatientEdit } from "./PatientEdit";
 export { default as PatientDetail } from "./PatientDetail";
 export { default as Patient } from "./Patient";
