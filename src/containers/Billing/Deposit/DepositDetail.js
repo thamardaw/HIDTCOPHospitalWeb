ae20d3913413a46887b89375c4130ab121b8a5c8
Zay Min Maw <zayminmaw77@gmail.com>
2022-01-19 15:57:42 +0630

    added print to deposit details

--- src/containers/Billing/Deposit/DepositDetail.js
+++ src/containers/Billing/Deposit/DepositDetail.js
@@ -1,16 +1,28 @@
-import { Divider, IconButton, Toolbar, Typography } from "@mui/material";
+import {
+  Button,
+  Divider,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { Box } from "@mui/system";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import { useHistory, useParams } from "react-router";
 import { useAxios } from "../../../hooks";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { generateID } from "../../../utils/generateID";
+import { useReactToPrint } from "react-to-print";
 
 const CategoryDetail = () => {
   const history = useHistory();
   const { id } = useParams();
   const api = useAxios();
   const [details, setDetails] = useState({});
+  const depositRef = useRef();
+
+  const handlePrint = useReactToPrint({
+    content: () => depositRef.current,
+  });
 
   const getData = async () => {
     const res = await api.get(`/api/deposit/${parseInt(id)}`);
@@ -52,10 +64,113 @@ const CategoryDetail = () => {
         >
           <ArrowBackIosNewIcon size="small" />
         </IconButton>
-        <Typography variant="h5">Details</Typography>
+        <Typography variant="h5" sx={{ flexGrow: 1 }}>
+          Details
+        </Typography>
+        <Button variant="contained" size="small" onClick={handlePrint}>
+          Print
+        </Button>
       </Toolbar>
       <Divider />
-      <Box sx={{ flexDirection: "column", padding: "20px 10px" }}>
+      <Box ref={depositRef} sx={{ display: "bock" }}>
+        <Typography variant="h6" textAlign="center" sx={{ marginTop: "20px" }}>
+          Dagon Lin Hospital
+        </Typography>
+        <Typography
+          variant="h6"
+          textAlign="center"
+          sx={{ fontWeight: "100", fontSize: "16px" }}
+        >
+          Receipt
+        </Typography>
+        <Box sx={{ flexDirection: "column", padding: "20px 10px" }}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              margin: "20px 0px",
+            }}
+          >
+            <Box sx={{ width: "30%" }}>
+              <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+                Date
+              </Typography>
+            </Box>
+            <Typography variant="body2">
+              {details?.created_time && details.created_time.split("T")[0]}
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              margin: "20px 0px",
+            }}
+          >
+            <Box sx={{ width: "30%" }}>
+              <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+                Deposit ID
+              </Typography>
+            </Box>
+            <Typography variant="body2">{details?.id && details.id}</Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              margin: "20px 0px",
+            }}
+          >
+            <Box sx={{ width: "30%" }}>
+              <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+                Patient ID
+              </Typography>
+            </Box>
+            <Typography variant="body2">
+              {details?.patient_id &&
+                generateID(details?.patient_id, details.patient.created_time)}
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              margin: "20px 0px",
+            }}
+          >
+            <Box sx={{ width: "30%" }}>
+              <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+                Patient Name
+              </Typography>
+            </Box>
+            <Typography variant="body2">
+              {details?.patient && details.patient.name}
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              margin: "20px 0px",
+            }}
+          >
+            <Box sx={{ width: "30%" }}>
+              <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+                Amount
+              </Typography>
+            </Box>
+            <Box sx={{ width: "70%" }}>
+              <Typography variant="body2">{details?.amount}</Typography>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+      {/* <Box sx={{ flexDirection: "column", padding: "20px 10px" }}>
         <Box
           sx={{
             display: "flex",
@@ -110,7 +225,7 @@ const CategoryDetail = () => {
             <Typography variant="body2">{details?.amount}</Typography>
           </Box>
         </Box>
-      </Box>
+      </Box> */}
       <Divider />
       <Box
         sx={{
