a307faf0cbbe3423abb4e4ee34ec2ccc8b624ce3
thuaung30 <thuhtetaung.d2@gmail.com>
2022-02-01 14:06:50 +0630

    Remove duplicate toDetailFromCancelled mehod

--- src/containers/Billing/Bills/BillsTable.js
+++ src/containers/Billing/Bills/BillsTable.js
@@ -98,10 +98,6 @@ const BillsTable = () => {
     );
   };
 
-  const toDetailFromCancelled = (id) => {
-    history.push(`/dashboard/bills/details/${id}/cancelled`);
-  };
-
   const getDraftedData = useCallback(async () => {
     setScreenLoading(true);
     const res = await api.get("/api/bill/drafted");
