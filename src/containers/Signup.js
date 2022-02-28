f4256a999ad8d7debc46030c0636981c1de390fd
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-02-28 16:20:44 +0630

    Update bill items order in print (#43)
    * removed unnecessary div
    
    * fixed mobile screen overflow in login, signup, reset password
    
    * added handle change function and fixed button loading bug
    
    * updated bill items order
    
    * prevented table header from repeating when page break

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -48,11 +48,6 @@ import LoadingButton from "@mui/lab/LoadingButton";
 //   alignItems: "center",
 // }));
 
-const Container = styled("div")(({ theme }) => ({
-  width: "100vw",
-  height: "100vh",
-}));
-
 const StyledBox = styled(Box)(({ theme }) => ({
   position: "absolute",
   top: 0,
@@ -80,7 +75,7 @@ const StyledPaper = styled(Paper)(({ theme }) => ({
   flexDirection: "column",
   alignItems: "center",
   [theme.breakpoints.down("sm")]: {
-    height: "92%",
+    boxShadow: "none",
   },
 }));
 
@@ -101,12 +96,17 @@ const Signup = () => {
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const handleChange = (e) => {
+    setDetails({ ...details, [e.target.name]: e.target.value });
+  };
+
   const submitHandler = async (e) => {
     setLoading(true);
     e.preventDefault();
     if (details.password !== details.confirmPassword) {
       message({ status: "error", detail: "Passwords do not match." });
       openAlert(true);
+      setLoading(false);
       return;
     }
     const res = await axios.post(
@@ -133,104 +133,95 @@ const Signup = () => {
     setLoading(false);
   };
   return (
-    <Container>
-      <StyledBox>
-        <StyledPaper elevation={6}>
-          <form onSubmit={submitHandler}>
-            <FormControl
-              fullWidth
-              required
-              variant="standard"
-              sx={{ marginBottom: "10px" }}
-            >
-              <InputLabel>Username</InputLabel>
-              <Input
-                onChange={(e) =>
-                  setDetails({ ...details, username: e.target.value })
-                }
-              />
-            </FormControl>
-            <FormControl
-              fullWidth
-              required
-              variant="standard"
-              sx={{ marginBottom: "10px" }}
-            >
-              <InputLabel>Password</InputLabel>
-              <Input
-                type={showPassword ? "text" : "password"}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={() => setShowPassword(!showPassword)}
-                    >
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-                onChange={(e) =>
-                  setDetails({ ...details, password: e.target.value })
-                }
-              />
-            </FormControl>
-            <FormControl
-              fullWidth
-              required
-              variant="standard"
-              sx={{ marginBottom: "10px" }}
-            >
-              <InputLabel>Confirm Password</InputLabel>
-              <Input
-                type={showConfirmPassword ? "text" : "password"}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={() =>
-                        setShowConfirmPassword(!showConfirmPassword)
-                      }
-                    >
-                      {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-                onChange={(e) =>
-                  setDetails({ ...details, confirmPassword: e.target.value })
-                }
-              />
-            </FormControl>
-            <TextField
-              select
-              required
-              fullWidth
-              label="Role"
-              variant="standard"
-              sx={{ marginBottom: "10px" }}
-              value={details.role}
-              onChange={(e) => setDetails({ ...details, role: e.target.value })}
-            >
-              <MenuItem value="Admin">Admin</MenuItem>
-              <MenuItem value="Receptionist">Receptionist</MenuItem>
-            </TextField>
-            <LoadingButton
-              fullWidth
-              loading={loading}
-              variant="contained"
-              style={{ marginTop: "20px" }}
-              type="submit"
-            >
-              Sign up
-            </LoadingButton>
-          </form>
-          <Button size="small" style={{ marginTop: "10px" }}>
-            <Typography sx={{ fontSize: "12px", fontWeight: "bold" }}>
-              <StyledLink to="/login">Already have an account?</StyledLink>
-            </Typography>
-          </Button>
-        </StyledPaper>
-      </StyledBox>
-    </Container>
+    <StyledBox>
+      <StyledPaper elevation={6}>
+        <form onSubmit={submitHandler}>
+          <FormControl
+            fullWidth
+            required
+            variant="standard"
+            sx={{ marginBottom: "10px" }}
+          >
+            <InputLabel>Username</InputLabel>
+            <Input name="username" onChange={handleChange} />
+          </FormControl>
+          <FormControl
+            fullWidth
+            required
+            variant="standard"
+            sx={{ marginBottom: "10px" }}
+          >
+            <InputLabel>Password</InputLabel>
+            <Input
+              type={showPassword ? "text" : "password"}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              name="password"
+              onChange={handleChange}
+            />
+          </FormControl>
+          <FormControl
+            fullWidth
+            required
+            variant="standard"
+            sx={{ marginBottom: "10px" }}
+          >
+            <InputLabel>Confirm Password</InputLabel>
+            <Input
+              type={showConfirmPassword ? "text" : "password"}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                  >
+                    {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              name="confirmPassword"
+              onChange={handleChange}
+            />
+          </FormControl>
+          <TextField
+            select
+            required
+            fullWidth
+            label="Role"
+            variant="standard"
+            sx={{ marginBottom: "10px" }}
+            value={details.role}
+            name="role"
+            onChange={handleChange}
+          >
+            <MenuItem value="Admin">Admin</MenuItem>
+            <MenuItem value="Receptionist">Receptionist</MenuItem>
+          </TextField>
+          <LoadingButton
+            fullWidth
+            loading={loading}
+            variant="contained"
+            style={{ marginTop: "20px" }}
+            type="submit"
+          >
+            Sign up
+          </LoadingButton>
+        </form>
+        <Button size="small" style={{ marginTop: "10px" }}>
+          <Typography sx={{ fontSize: "12px", fontWeight: "bold" }}>
+            <StyledLink to="/login">Already have an account?</StyledLink>
+          </Typography>
+        </Button>
+      </StyledPaper>
+    </StyledBox>
   );
 };
 
