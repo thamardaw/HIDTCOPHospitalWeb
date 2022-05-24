6b4411622a076586e21a58705e336e5908e77e66
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-05-24 11:31:34 +0630

    feat(inventory_item): added inventory item detail

--- src/containers/Inventory/InventoryTransaction/InventoryTransactionTable.js
+++ src/containers/Inventory/InventoryTransaction/InventoryTransactionTable.js
@@ -1,3 +1,66 @@
+const headCells = [
+  {
+    id: "id",
+    numeric: false,
+    disablePadding: true,
+    label: "ID",
+  },
+  {
+    id: "brand_name",
+    numeric: false,
+    disablePadding: false,
+    label: "Brand Name",
+  },
+  {
+    id: "generic_name",
+    numeric: false,
+    disablePadding: false,
+    label: "Generic Name",
+  },
+  {
+    id: "batch",
+    numeric: false,
+    disablePadding: false,
+    label: "Batch",
+  },
+  {
+    id: "transaction_type",
+    numeric: false,
+    disablePadding: false,
+    label: "Transaction Type",
+  },
+  {
+    id: "quantity",
+    numeric: false,
+    disablePadding: false,
+    label: "Quantity",
+  },
+  {
+    id: "unit",
+    numeric: false,
+    disablePadding: false,
+    label: "Unit",
+  },
+  {
+    id: "purchasing_price",
+    numeric: false,
+    disablePadding: false,
+    label: "Purchasing Price",
+  },
+  {
+    id: "selling_price",
+    numeric: false,
+    disablePadding: false,
+    label: "Selling Price",
+  },
+  {
+    id: "total",
+    numeric: false,
+    disablePadding: false,
+    label: "Total",
+  },
+];
+
 const InventoryTransactionTable = () => {
   return <h1>Inventory tac Table</h1>;
 };
