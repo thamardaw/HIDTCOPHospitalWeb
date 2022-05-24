d43eab85a7bf2ac6a603cf97ed1d32aea67f977a
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-05-24 08:52:19 +0630

    feat(pharmacy_item): added table ui

--- src/containers/Inventory/PharmacyItem/PharmacyItemTable.js
+++ src/containers/Inventory/PharmacyItem/PharmacyItemTable.js
@@ -1,5 +1,74 @@
+import { useContext, useState } from "react";
+import { CustomTable } from "../../../components";
+import { LoadingContext } from "../../../contexts";
+import { useAxios } from "../../../hooks";
+
+const headCells = [
+  {
+    id: "id",
+    numeric: false,
+    disablePadding: true,
+    label: "ID",
+  },
+  {
+    id: "brand_name",
+    numeric: false,
+    disablePadding: false,
+    label: "Brand Name",
+  },
+  {
+    id: "generic_name",
+    numeric: false,
+    disablePadding: false,
+    label: "Generic Name",
+  },
+  {
+    id: "form",
+    numeric: false,
+    disablePadding: false,
+    label: "Form",
+  },
+  {
+    id: "strength",
+    numeric: false,
+    disablePadding: false,
+    label: "Strength",
+  },
+  {
+    id: "po_unit",
+    numeric: false,
+    disablePadding: false,
+    label: "PO-unit",
+  },
+  {
+    id: "unit",
+    numeric: false,
+    disablePadding: false,
+    label: "Unit",
+  },
+];
+
 const PharmacyItemTable = () => {
-  return <h1>Pharmacy Item Table</h1>;
+  const api = useAxios({ autoSnackbar: true });
+  const [rows, setRows] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState([]);
+  const { setScreenLoading } = useContext(LoadingContext);
+
+  const handleClickOpen = (arr) => {
+    setSelected(arr);
+    setOpen(true);
+  };
+
+  return (
+    <CustomTable
+      tableName="Pharmacy Item"
+      headCells={headCells}
+      rows={rows}
+      onDelete={handleClickOpen}
+      enableMultipleDelete={false}
+    />
+  );
 };
 
 export default PharmacyItemTable;
