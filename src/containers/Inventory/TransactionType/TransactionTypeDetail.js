63c616b06ef94b3e705fa417126ec85851815a02
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-06-11 13:29:52 +0630

    feat(transaction type details): api integrated

--- src/containers/Inventory/TransactionType/TransactionTypeDetail.js
+++ src/containers/Inventory/TransactionType/TransactionTypeDetail.js
@@ -1,16 +1,31 @@
 import { Box, Divider, Toolbar, Typography } from "@mui/material";
-// import { useState } from "react";
-// import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 import { BackButton, DetailsRow } from "../../../components";
-// import { useAxios } from "../../../hooks";
+import { useAxios } from "../../../hooks";
 
 const TransactionTypeDetail = () => {
   const history = useHistory();
-  // const { id } = useParams();
-  // const api = useAxios({ autoSnackbar: true });
-  // const [details, setDetails] = useState({});
-  const details = {};
+  const { id } = useParams();
+  const api = useAxios({ autoSnackbar: true });
+  const [details, setDetails] = useState({});
+
+  const getData = async () => {
+    const res = await api.get(`/api/transaction_types/${parseInt(id)}`);
+    if (res.status === 200) {
+      setDetails({ ...res.data });
+    }
+  };
+
+  useEffect(() => {
+    if (id) {
+      getData();
+    } else {
+      history.goBack();
+    }
+    // eslint-disable-next-line
+  }, [id]);
 
   return (
     <Box sx={{ flexGrow: 1, mb: 1 }}>
