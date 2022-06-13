e6254a5be3bb96accc3cb9f98ff9f46869bde0a9
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-06-14 00:14:43 +0630

    feat(inventory item details): api integrated

--- src/containers/Inventory/InventoryItem/InventoryItemDetail.js
+++ src/containers/Inventory/InventoryItem/InventoryItemDetail.js
@@ -1,16 +1,31 @@
 import { Box, Divider, Toolbar, Typography } from "@mui/material";
-// import { useState } from "react";
-// import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 import { BackButton, DetailsRow } from "../../../components";
-// import { useAxios } from "../../../hooks";
+import { useAxios } from "../../../hooks";
 
 const InventoryItemDetail = () => {
   const history = useHistory();
-  // const { id } = useParams();
-  // const api = useAxios({ autoSnackbar: true });
-  // const [details, setDetails] = useState({});
-  const details = {};
+  const { id } = useParams();
+  const api = useAxios({ autoSnackbar: true });
+  const [details, setDetails] = useState({});
+
+  const getData = async () => {
+    const res = await api.get(`/api/inventory_items/${parseInt(id)}`);
+    if (res.status === 200) {
+      setDetails({ ...res.data });
+    }
+  };
+
+  useEffect(() => {
+    if (id) {
+      getData();
+    } else {
+      history.goBack();
+    }
+    // eslint-disable-next-line
+  }, [id]);
 
   return (
     <Box sx={{ flexGrow: 1, mb: 1 }}>
@@ -27,9 +42,15 @@ const InventoryItemDetail = () => {
       </Toolbar>
       <Divider />
       <Box sx={{ flexDirection: "column", padding: "20px 10px" }}>
-        <DetailsRow name="Brand Name" value={details?.brand_name} />
+        <DetailsRow
+          name="Brand Name"
+          value={details?.pharmacy_item?.brand_name}
+        />
         <Divider />
-        <DetailsRow name="Generic Name" value={details?.generic_name} />
+        <DetailsRow
+          name="Generic Name"
+          value={details?.pharmacy_item?.generic_name}
+        />
         <Divider />
         <DetailsRow name="Balance" value={details?.balance} />
         <Divider />
@@ -41,7 +62,10 @@ const InventoryItemDetail = () => {
         <Divider />
         <DetailsRow name="Purchasing Price" value={details?.purchasing_price} />
         <Divider />
-        <DetailsRow name="Sales Item" value={details?.selling_price} />
+        <DetailsRow
+          name="Sales Item"
+          value={details?.sales_service_item?.name}
+        />
       </Box>
       <Divider />
     </Box>
