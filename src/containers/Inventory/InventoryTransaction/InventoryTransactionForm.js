325dbd1c115ca75c722d22bee59bb3f1bb7fe59a
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-06-16 14:00:20 +0630

    feat(inventory transaction form): api integrated

--- src/containers/Inventory/InventoryTransaction/InventoryTransactionForm.js
+++ src/containers/Inventory/InventoryTransaction/InventoryTransactionForm.js
@@ -3,27 +3,62 @@ import {
   Autocomplete,
   Box,
   Divider,
+  MenuItem,
   TextField,
   Toolbar,
   Typography,
 } from "@mui/material";
-import { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { BackButton } from "../../../components";
-// import { useAxios } from "../../../hooks";
+import { useAxios } from "../../../hooks";
 
 const InventoryTransactionForm = () => {
   const history = useHistory();
-  const { id } = useParams();
-  // const api = useAxios({ autoSnackbar: true });
-  // const [loading, setLoading] = useState(false);
-  const loading = false;
+  const api = useAxios({ autoSnackbar: true });
+  const [loading, setLoading] = useState(false);
+  const [inventoryItems, setInventoryItems] = useState([]);
+  const [transactionTypes, setTransactionTypes] = useState([]);
   const [details, setDetails] = useState({});
 
   const handleChange = (e) => {
     setDetails({ ...details, [e.target.name]: e.target.value });
   };
+
+  const getInventoryItemsAndTransactionType = async () => {
+    const [invi, txt] = await Promise.all([
+      api.get("/api/inventory_items/"),
+      api.get("/api/transaction_types/"),
+    ]);
+    if (invi.status === 200 && txt.status === 200) {
+      setInventoryItems(invi.data);
+      setTransactionTypes(txt.data);
+    } else {
+      history.goBack();
+    }
+    return;
+  };
+
+  const createNew = async () => {
+    setLoading(true);
+    const txt = transactionTypes.find(
+      (t) => t.id === details.transaction_type_id
+    );
+    const res = await api.post("/api/inventory_transactions/", {
+      ...details,
+      transaction_type_name: txt.name,
+      transaction_type: txt.type,
+    });
+    if (res.status === 200) {
+      history.goBack();
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getInventoryItemsAndTransactionType();
+    // eslint-disable-next-line
+  }, []);
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -36,7 +71,7 @@ const InventoryTransactionForm = () => {
         disableGutters={true}
       >
         <BackButton backFunction={() => history.goBack()} />
-        <Typography variant="h5">{id ? "Edit" : "New"}</Typography>
+        <Typography variant="h5">New</Typography>
       </Toolbar>
       <Divider />
       <Box sx={{ flexDirection: "column", padding: "20px 10px" }}>
@@ -51,12 +86,23 @@ const InventoryTransactionForm = () => {
             <Typography variant="p">Inventory Item</Typography>
           </Box>
           <Autocomplete
-            options={[]}
+            options={inventoryItems}
             style={{ width: "70%" }}
             getOptionLabel={(option) => option.name}
+            isOptionEqualToValue={(option, value) => option.id === value.id}
+            onChange={(e, value) => {
+              setDetails({
+                ...details,
+                inventory_item_id: value.id,
+                inventory_item_name: value.name,
+                unit: value.unit,
+                purchasing_price: value.purchasing_price,
+                selling_price: value?.sales_service_item?.price,
+              });
+            }}
             renderOption={(props, option) => {
               return (
-                <Box {...props} key={option.sales_service_item_id}>
+                <Box {...props} key={option.id}>
                   {option.name}
                 </Box>
               );
@@ -85,15 +131,19 @@ const InventoryTransactionForm = () => {
           <TextField
             select
             fullWidth
-            label="Unit"
+            label="Type"
             size="small"
             sx={{ width: "70%" }}
             margin="dense"
-            value={details?.transaction_type || ""}
-            name="transaction_type"
+            value={details?.transaction_type_id || ""}
+            name="transaction_type_id"
             onChange={handleChange}
           >
-            {/* <MenuItem value="male">Male</MenuItem> */}
+            {transactionTypes.map((txt) => (
+              <MenuItem key={txt.id} value={txt.id}>
+                {txt.name}
+              </MenuItem>
+            ))}
           </TextField>
         </Box>
         <Box
@@ -132,8 +182,8 @@ const InventoryTransactionForm = () => {
             margin="dense"
             multiline
             rows={3}
-            value={details?.selling_price || ""}
-            name="selling_price"
+            value={details?.note || ""}
+            name="note"
             onChange={handleChange}
           />
         </Box>
@@ -153,7 +203,7 @@ const InventoryTransactionForm = () => {
           loading={loading}
           size="small"
           sx={{ marginRight: "5px" }}
-          // onClick={id ? update : createNew}
+          onClick={createNew}
         >
           Save
         </LoadingButton>
