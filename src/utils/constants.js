644509528ce404eb2dd39cf49d4f3b0c287537c9
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-06-19 10:34:12 +0630

    fix: name changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const constants = {
-  name: process.env.REACT_APP_TITLE || "HIDTCOP",
+  name: process.env.REACT_APP_TITLE || "Genesis",
   hospital_name: `${process.env.REACT_APP_TITLE || "Dagon Lin"} Hospital`,
   name_symbol: process.env.REACT_APP_TITLE_SHORT || "DGL",
   primary_color: process.env.REACT_APP_THEME || "#1976d2",
